70393d296b2588cb9f8cd9b304691af56da22296
guangcong.lgc <guangcong.lgc@cainiao.com>
2016-07-17 16:04:08 +0800

    apply implicitly

--- src/main/scala/org/cup/collection/immutable/Pair.scala
+++ src/main/scala/org/cup/collection/immutable/Pair.scala
@@ -2,6 +2,7 @@ package org.cup.collection.immutable
 
 import scala.math.Ordering
 
-case class Pair[T : Ordering](first: T, second: T) {
-  def smaller = Ordering[T].min(first, second)
+case class Pair[+T : Ordering](first: T, second: T) {
+  //def smaller = Ordering[T].min(first, second)
+  def smaller = implicitly[Ordering[T]].min(first, second)
 }
