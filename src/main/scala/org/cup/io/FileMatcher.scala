e2543b856648e651fef4e4d3995a9404eb4df56e
guangcong.lgc <guangcong.lgc@cainiao.com>
2016-07-17 14:36:25 +0800

    add FileMatcher

---
+++ src/main/scala/org/cup/io/FileMatcher.scala
@@ -0,0 +1,19 @@
+package org.cup.io
+
+import java.io.File
+
+object FileMatcher {
+  private type Matcher = String => Boolean
+
+  private def list(file: File)(matcher: Matcher) =
+    file.listFiles.filter(f => matcher(f.getName))
+
+  def ends(file: File, ext: String) =
+    list(file) { _.endsWith(ext) }
+
+  def contains(file: File, query: String) =
+    list(file) { _.contains(query) }
+
+  def matches(file: File, regex: String) =
+    list(file) { _.matches(regex) }
+}
